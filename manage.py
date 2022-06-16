e0fea58983cf9e1af9189da8c6e3812254482fab
Qasim Khan <69918464+QasimAK191@users.noreply.github.com>
2022-06-16 22:05:47 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+
 import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings.deployment')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
